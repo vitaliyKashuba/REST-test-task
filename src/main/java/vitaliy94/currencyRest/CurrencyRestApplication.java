49e7b426fb5277edfd34b7153c1c617f101d548f
Vitaliy <k.vitaliy1994@gmail.com>
2019-06-26 10:20:06 +0300

    init

---
+++ src/main/java/vitaliy94/currencyRest/CurrencyRestApplication.java
@@ -0,0 +1,13 @@
+package vitaliy94.currencyRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRestApplication.class, args);
+	}
+
+}
